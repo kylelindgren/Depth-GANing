3b14d08f2a951529b99fa955588b8c5bdbd7745a
titan <kylelindgren3@gmail.com>
2018-05-24 13:59:13 -0700

    cleaned variable names

--- process_princeton_depth.py
+++ process_princeton_depth.py
@@ -39,28 +39,14 @@ def extract_sams():
 				if e.errno != errno.EEXIST:
 					raise
 
-			k1_d = cv2.imread(folder_imgs[i], cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
+			img = cv2.imread(folder_imgs[i], cv2.IMREAD_ANYDEPTH | cv2.IMREAD_ANYCOLOR)
 
-			k1_d[k1_d > 4000] = 0  # out of range = black in NYU, 4000mm -> kinect range ~ 4m
-			cvt = (255*(k1_d/4000)).astype('uint8')
+			img[img > 4000] = 0  # out of range = black in NYU, 4000mm -> kinect range ~ 4m
+			img = (255*(img/4000)).astype('uint8')
+
+			img = cv2.resize(img[:, 80:560], dsize=(128, 128))
 			
-			# print(k1_d.dtype)
-			# print(cvt.dtype)
-			# print(np.max(np.max(cvt)))
-			# print(np.min(np.min(cvt)))
-			# print(k1_d.shape)  # (480, 640)
-
-			cvt = cv2.resize(cvt[:, 80:560], dsize=(128, 128))  # orig (146, 202)
-			# gt_d = cv2.resize(np.uint8(gt_d[:, 28:174]), dsize=(128, 128))
-			# noise = np.zeros(k1_d.shape, np.uint8)
-			# noise[k1_d < 3] = 255
-			# vis = np.concatenate((gt_d, noise), axis=1)
-
-			# cv2.imshow('img', cvt)
-			# cv2.waitKey(200)
-			
-			# vis = cv2.cvtColor(vis, cv2.COLOR_GRAY2RGB)
-			cv2.imwrite(out_fld + str(idx) + '.png', cvt)
+			cv2.imwrite(out_fld + str(idx) + '.png', img)
 			idx = idx + 1
 
 if __name__ == '__main__':
